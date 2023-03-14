ac5dead57535c2eee688ee3dc5deba15b8a42261
vivek <vivekanandpv@outlook.com>
2023-03-14 14:41:03 +0530

    Initial commit

---
+++ src/main/java/com/vivekanandpv/jpademo/models/Car.java
@@ -0,0 +1,76 @@
+package com.vivekanandpv.jpademo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String make;
+    private String model;
+    private int bhp;
+    private int torque;
+    private String color;
+    private String fuel;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getBhp() {
+        return bhp;
+    }
+
+    public void setBhp(int bhp) {
+        this.bhp = bhp;
+    }
+
+    public int getTorque() {
+        return torque;
+    }
+
+    public void setTorque(int torque) {
+        this.torque = torque;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+}
---
+++ src/test/java/com/vivekanandpv/jpademo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.jpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
