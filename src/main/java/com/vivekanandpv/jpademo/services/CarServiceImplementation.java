0b5a6df0626a2eb0f983f3c70aebfec62934b94a
vivek <vivekanandpv@outlook.com>
2023-03-14 16:06:53 +0530

    service implementation

--- src/main/java/com/vivekanandpv/jpademo/services/CarServiceImplementation.java
+++ src/main/java/com/vivekanandpv/jpademo/services/CarServiceImplementation.java
@@ -1,12 +1,17 @@
 package com.vivekanandpv.jpademo.services;
 
+import com.vivekanandpv.jpademo.exceptions.RecordNotFoundException;
+import com.vivekanandpv.jpademo.models.Car;
 import com.vivekanandpv.jpademo.repositories.CarRepository;
 import com.vivekanandpv.jpademo.viewmodels.CarCreateViewModel;
 import com.vivekanandpv.jpademo.viewmodels.CarUpdateViewModel;
 import com.vivekanandpv.jpademo.viewmodels.CarViewModel;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImplementation implements CarService {
@@ -18,26 +23,60 @@ public class CarServiceImplementation implements CarService {
 
     @Override
     public List<CarViewModel> getAll() {
-        return null;
+        return repository
+                .findAll()
+                .stream()
+                .map(c -> {
+                    CarViewModel vm = new CarViewModel();
+                    BeanUtils.copyProperties(c, vm);
+                    return vm;
+                })
+                .collect(Collectors.toList());
     }
 
     @Override
     public CarViewModel getById(int id) {
-        return null;
+        Car car = repository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(String.format("Could not find the car with id: %d", id)));
+
+        CarViewModel viewModel = new CarViewModel();
+        BeanUtils.copyProperties(car, viewModel);
+
+        return viewModel;
     }
 
     @Override
     public CarViewModel create(CarCreateViewModel viewModel) {
-        return null;
+        Car car = new Car();
+        BeanUtils.copyProperties(viewModel, car);
+        repository.saveAndFlush(car);
+
+        CarViewModel carViewModel = new CarViewModel();
+        BeanUtils.copyProperties(car, carViewModel);
+
+        return carViewModel;
     }
 
     @Override
     public CarViewModel update(int id, CarUpdateViewModel viewModel) {
-        return null;
+        Car car = repository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(String.format("Could not find the car with id: %d", id)));
+
+        BeanUtils.copyProperties(viewModel, car);
+
+        repository.saveAndFlush(car);
+
+        CarViewModel carViewModel = new CarViewModel();
+        BeanUtils.copyProperties(car, carViewModel);
+
+        return carViewModel;
     }
 
     @Override
     public void deleteById(int id) {
+        Car car = repository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(String.format("Could not find the car with id: %d", id)));
 
+        repository.delete(car);
     }
 }
